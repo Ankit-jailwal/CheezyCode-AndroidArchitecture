3dc30f0a28f9f470f9f98a9ac1d92016d2444747
Ankit-jailwal <jailwalankit@gmail.com>
2023-04-07 06:40:44 +0530

    ViewModel example

--- app/src/main/java/com/example/cheezycode/MainActivity.kt
+++ app/src/main/java/com/example/cheezycode/MainActivity.kt
@@ -3,16 +3,38 @@ package com.example.cheezycode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtCounter : TextView
+    lateinit var button : Button
+
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel = androidx.lifecycle.ViewModelProvider(this).get(MainViewModel::class.java)
         val observer = Observer()
         lifecycle.addObserver(observer)
-        Log.d("main", "Main activity oncreate")
+        txtCounter = findViewById(R.id.count)
+        button = findViewById(R.id.button)
+        setCount()
     }
 
+
+
+    fun setCount() {
+        txtCounter.text = mainViewModel.count.toString()
+
+    }
+
+    fun increment (v: View) {
+        mainViewModel.increment()
+        setCount()
+    }
     override fun onResume() {
         super.onResume()
         Log.d("main", "Main activity onResume")
